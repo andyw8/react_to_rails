51f995cea741b28549f4c7fcfead9877dc3a26af
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-06-09 09:08:05 -0400

    Initial commit
    $ bundle gem --github-username=andyw8 --no-ext --linter=standard react_to_rails

---
+++ lib/react_to_rails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "react_to_rails/version"
+
+module ReactToRails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_react_to_rails.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestReactToRails < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ReactToRails::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
