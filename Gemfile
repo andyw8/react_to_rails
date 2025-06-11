034bc4def99722dff4137302d289dcff07f043a6
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-06-10 20:15:55 -0400

    Add approval test

--- Gemfile
+++ Gemfile
@@ -11,3 +11,4 @@ gem "rake", "~> 13.0"
 gem "minitest", "~> 5.16"
 
 gem "standard", "~> 1.3"
+gem "erb-formatter"
--- test/test_react_to_rails.rb
+++ test/test_react_to_rails.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "erb/formatter"
 
 class TestReactToRails < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ReactToRails::VERSION
   end
+
+  def test_approvals
+    expected_rb = ERB::Formatter.format(File.read("test/approvals/pricing_component.rb"))
+    expected_erb = ERB::Formatter.format(File.read("test/approvals/pricing_component.html.erb"))
+
+    convert = ReactToRails::Convert.for_path("examples/pricing.jsx", name: "Pricing")
+    convert.call
+
+    actual_rb = ERB::Formatter.format(convert.view_component_ruby_code)
+    actual_erb = ERB::Formatter.format(convert.erb_template)
+
+    assert_equal expected_rb, actual_rb
+    assert_equal expected_erb, actual_erb
+  end
+
+  # formatted = ERB::Formatter.format <<-ERB
 end
